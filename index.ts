c47a6039acb30754ad39f0b0d6c6e342b9075196
Peter Pang <mv@live.cn>
2019-10-15 18:14:28 +0800

    Change ES6 export to module.export.

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
-export default {
+module.exports = {
+  'menu-dark-submenu-bg': '#147465',
   'primary-color': '#1DA57A',
+  'layout-header-background': '#147465',
+  'tooltip-bg': 'rgba(20, 116, 101, 0.8);',
+  'tooltip-color': '#147465'
 };
